f7a5f7738502f5db1c133a446be10f25ae1ec01f
Benjamin G Nechicattu <benjaminnechicattu@gmail.com>
2019-10-28 10:07:37 +0530

    Changed name

--- youngest.c
+++ youngest.c
@@ -3,22 +3,22 @@
 #include<stdio.h>
 void main()
 {
-int age_ram,age_shyam,age_ajay,young;
+int age_sam,age_shyam,age_ajay,young;
 printf("enter age of ram:");
-scanf("%d",&age_ram);
+scanf("%d",&age_sam);
 printf("enter age of shyam:");
 scanf("%d",&age_shyam);
 printf("enter age of ajay:");
 scanf("%d",&age_ajay);
 
-young=age_ram;
-if(young<age_shyam&&age_ram<age_ajay)
-	printf("ram is youngest:");
+young=age_sam;
+if(young<age_shyam&&age_sam<age_ajay)
+	printf("sam is youngest:");
 {
-if(age_shyam<age_ram&&age_shyam<age_ajay)
+if(age_shyam<age_sam&&age_shyam<age_ajay)
 	printf("shyam is youngest");
 }
-if(age_ajay<age_ram&&age_ajay<age_shyam)
+if(age_ajay<age_sam&&age_ajay<age_shyam)
 	printf("ajay is youngest");
 }
 
